9c1bdaa7cad337c1a223a13772fbce1f951c49e0
Grisenko Andrii <115999885+AndDemon@users.noreply.github.com>
2024-05-01 16:50:48 +0300

    Add files via upload

---
+++ src/domain/Employee.java
@@ -0,0 +1,155 @@
+package domain;
+
+/**
+ * Клас, що представляє працівника.
+ */
+public class Employee {
+
+    /**
+     * Повертає рядкове представлення працівника з його основними даними.
+     * 
+     * @return рядкове представлення працівника з ID, ім'ям, посадою, рівнем та відділом
+     */
+    @Override
+    public String toString() {
+        return "\nІдентифікатор працівника= " + ID + "\nІм'я= " + name + "\nПосада= " + jobTitle + "\nРівень= " + level + "\nВідділ= " + dept;
+    }
+
+    private String name;
+    private String jobTitle;
+    private int ID;
+    private int level;
+    private String dept;
+
+   // private static int employeesCount = 0;
+
+    /**
+     * Створює нового працівника з вказаними основними даними.
+     * 
+     * @param name ім'я працівника
+     * @param jobTitle посада працівника
+     * @param level рівень працівника
+     * @param dept відділ працівника
+     */
+    public Employee(String name, String jobTitle, int level, String dept) {
+        this();
+        setName(name);
+        this.jobTitle = jobTitle;
+        switch (level) {
+            case 1:
+            case 2:
+            case 3:
+                this.level = level;
+                break;
+            default:
+                this.level = 1;
+        }
+        this.dept = dept;
+    }
+
+    /**
+     * Створює нового працівника з пустими основними даними.
+     */
+    public Employee() {
+        ID = (int)(Math.random()*1000);
+//        if (ID > 10000) {
+//            throw new NumberFormatException("ERROR! Employee's ID cannot be greater than 10000. Employee is not created!");
+//        }
+    }
+
+    /**
+     * Встановлює посаду працівника.
+     * 
+     * @param job посада працівника
+     */
+    public void setJobTitle(String job) {
+        jobTitle = job;
+    }
+
+    /**
+     * Повертає посаду працівника.
+     * 
+     * @return посада працівника
+     */
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    /**
+     * Повертає ім'я працівника.
+     * 
+     * @return ім'я працівника
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Встановлює рівень працівника.
+     * 
+     * @param level рівень працівника
+     */
+    public void setLevel(int level) {
+        switch (level) {
+            case 1:
+            case 2:
+            case 3:
+                this.level = level;
+                break;
+            default:
+                this.level = 1;
+        }
+    }
+
+    /**
+     * Повертає рівень працівника.
+     * 
+     * @return рівень працівника
+     */
+    public int getLevel() {
+        return level;
+    }
+
+    /**
+     * Повертає відділ працівника.
+     * 
+     * @return відділ працівника
+     */
+    public String getDept() {
+        return dept;
+    }
+
+    /**
+     * Встановлює відділ працівника.
+     * 
+     * @param dept відділ працівника
+     */
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    /**
+     * Встановлює ім'я працівника з урахуванням деяких обмежень.
+     * 
+     * @param name ім'я працівника
+     */
+    public void setName(String name) {
+//        boolean correctName = true;
+//        for (int i = 0; i < name.length(); i++) {
+//            char s = name.charAt(i);
+//            if (Character.isDigit(s)) {
+//                correctName = false;
+//            }
+//        }
+//        if (correctName) {
+//            this.name = name;
+//        } else {
+//            this.name = "John Doe";
+//        }
+        if (name.matches("^([a-zA-Z]+[\\'\\,\\.\\-]?[a-zA-Z ]*)+[ ]([a-zA-Z]+[\\'\\,\\.\\-]?[a-zA-Z ]+)+$")) {
+            this.name = name;
+        } else {
+            this.name = "John Doe";
+        }
+    }
+}
